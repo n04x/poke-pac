c76eeaba359920242d4cb3b18548c3ebbcde9436
n04x <ThomasBacks@gmail.com>
2019-03-09 15:56:00 -0500

    Added Pikachu
    Added Pikachu prefab

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float speed = 5.0f;
+
+    public GameObject jelly_prefabs { get; set; }
+
+    private GameObject jelly;
+    private GameObject player_camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
