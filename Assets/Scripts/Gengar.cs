6f74f2e278aa68120c166488f9ec727ea27e4ffc
n04x <thomasbacks@gmail.com>
2019-03-11 18:01:45 -0400

    Added some scripts

--- Assets/Scripts/Gengar.cs
+++ Assets/Scripts/Gengar.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class Gengar : MonoBehaviour
 {
-    private UnityEngine.AI.NavMeshAgent nav_mesh_agent;
+    private NavMeshAgent nav_mesh_agent;
     private MeshCollider mesh_collider;
     //private PlayerList player_list;
 
     void Start() {
-        nav_mesh_agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        nav_mesh_agent = GetComponent<NavMeshAgent>();
         mesh_collider = GetComponentInChildren<MeshCollider>();
+        // player_list = GameObject.Find("PlayerList").GetComponent<PlayerList>();
+    }
 
+    void Update() {
+            
     }
 }
---
+++ Assets/Scripts/PokemonList.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PokemonList : MonoBehaviour
+{
+    private List<Pokemon> pokemon_list = new List<Pokemon>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameOver();    
+    }
+
+    void GameOver() {
+        if(pokemon_list.Count == 0) {
+            return;
+        }
+        foreach (Pokemon p in pokemon_list)
+        {
+            if(!p.is_dead) {
+                return;
+            }
+        }
+        // GameObject.FindObjectOfType<Stats>().pellet_count = 0;
+    }
+    
+    public void AddPokemon(Pokemon p) {
+        if(!pokemon_list.Contains(p)) {
+            pokemon_list.Add(p);
+        }
+    }
+
+    public ICollection<Pokemon> GetPokemon() {
+        return pokemon_list;
+    }
+}
