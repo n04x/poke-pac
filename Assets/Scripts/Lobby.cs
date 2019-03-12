d528935a7fae0b6d009bf1cd82afddf9e283e917
n04x <ThomasBacks@gmail.com>
2019-03-11 21:52:10 -0400

    Backup
    Added Lobby Scene for Network

---
+++ Assets/Scripts/Lobby.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Lobby : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private byte max_player = 4;
+    void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;    
+    }
+
+     public void Connect()
+    {
+        if(PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        } else
+        {
+            PhotonNetwork.GameVersion = "1";
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.LogWarningFormat("OnDisconnected() was called by PUN with reason {0}", cause);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = max_player });
+    }
+}
