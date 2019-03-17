b792ba221492a71a56af338f23dafddccb121c8f
n04x <ThomasBacks@gmail.com>
2019-03-17 03:16:29 -0400

    Fixed Collision and movement
    Fix some collision and movement deteciton

--- Assets/Scripts/Photon/GameController/PokemonMovement.cs
+++ Assets/Scripts/Photon/GameController/PokemonMovement.cs
@@ -6,14 +6,14 @@ using UnityEngine;
 public class PokemonMovement : MonoBehaviour
 {
     private PhotonView PV;
-    private CharacterController CC;
+    //private CharacterController CC;
     public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
         PV = GetComponent<PhotonView>();
-        CC = GetComponent<CharacterController>();
+        //CC = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -30,23 +30,33 @@ public class PokemonMovement : MonoBehaviour
         if(Input.GetKey(KeyCode.W))
         {
             this.transform.rotation = Quaternion.LookRotation(Vector3.forward, Vector3.up);
-            CC.Move(transform.forward * Time.deltaTime * speed);
+            this.GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + Vector3.forward * speed * Time.deltaTime);
+
         }
         if (Input.GetKey(KeyCode.A))
         {
             this.transform.rotation = Quaternion.LookRotation(Vector3.left, Vector3.up);
-            CC.Move(transform.forward * Time.deltaTime * speed);
+            this.GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position - Vector3.right * speed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.S))
         {
             this.transform.rotation = Quaternion.LookRotation(Vector3.back, Vector3.up);
-            CC.Move(transform.forward * Time.deltaTime * speed);
+            this.GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position - Vector3.forward * speed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.D))
         {
             this.transform.rotation = Quaternion.LookRotation(Vector3.right, Vector3.up);
-            CC.Move(transform.forward * Time.deltaTime * speed);
+            this.GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + Vector3.right * speed * Time.deltaTime);
         }
-
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "pokeball")
+        {
+            collision.gameObject.SetActive(false);
+        } else
+        {
+            return;
+        }
     }
 }
