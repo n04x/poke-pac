ceb8702324f5d720a91b9d0898239945b1262303
n04x <thomasbacks@gmail.com>
2019-03-18 13:31:17 -0400

    added a timer to the game.

--- Assets/Scripts/Photon/GameController/GameSetup.cs
+++ Assets/Scripts/Photon/GameController/GameSetup.cs
@@ -11,6 +11,15 @@ public class GameSetup : MonoBehaviour
 
     public Transform[] spawn_positions;
     public Text scores;
+    public Text timer;
+    public float time_left = 240.0f;    // 4 minutes.
+    private float minutes;
+    private float seconds;
+
+    private void Update() {
+        GetTimer();
+        timer.text = string.Format("{0:0}:{1:00}", minutes, seconds);
+    }
 
     private void OnEnable()
     {
@@ -33,5 +42,18 @@ public class GameSetup : MonoBehaviour
             yield return null;
         }
         SceneManager.LoadScene(MultiplayerSetting.mp_setting.menu_scene);
+    }
+
+    void GetTimer() {
+        time_left -= Time.deltaTime;
+        minutes = Mathf.Floor(time_left / 60);
+        seconds = time_left % 60;
+        if(seconds > 59) {
+            seconds = 59;
+        }
+        if(minutes < 0) {
+            minutes = 0.0f;
+        }
+
     }
 }
