b8dc78b4797ff94a44c896c2b72bbc3e47a91bb2
n04x <ThomasBacks@gmail.com>
2019-03-22 21:38:08 -0400

    Cleanup
    Cleanup of the project

---
+++ Assets/Scripts/PokePuffBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PokePuffBehaviour : MonoBehaviour
+{
+    SkinnedMeshRenderer[] meshes;
+    public float spawn_timer = 30.0f;   // spawn the pokebuff every 30 seconds.
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshes = GetComponentsInChildren<SkinnedMeshRenderer>();
+        foreach (SkinnedMeshRenderer sm in meshes)
+        {
+            sm.enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Spawner();
+    }
+    void Spawner() {
+        spawn_timer -= Time.deltaTime;
+        if(spawn_timer <= 0) {
+            foreach (SkinnedMeshRenderer sm in meshes)
+            {
+                sm.enabled = true;
+            }
+        }
+    }
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.tag == "Player") {
+            GameSetup.GS.pokepuff_eaten = true;
+            foreach (SkinnedMeshRenderer sm in meshes)
+            {
+                sm.enabled = false;
+            }
+        } else
+        {
+            return;
+        }
+    }
+}
